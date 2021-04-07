81f1806517113cde23b69b3cfbbe4d55aa3b2718
AmirAdarGit <amiradar12345@gmail.com>
2021-04-07 22:29:26 +0300

    video and image components

--- my-app/src/components/VideoGallery.js
+++ my-app/src/components/VideoGallery.js
@@ -3,20 +3,18 @@ import { useDispatch, useSelector } from "react-redux"
 import Image from "./Image"
 
 function VideoGallery() {
-    const cases = useSelector((state) => state.useCases.useCases);
     const videosGallery = useSelector((state) => state.videoGallery.previewImages);
+    const title = useSelector((state) => state.videoGallery.title);
     console.log(`video gallery: ${videosGallery}`);
     return (
-        
-        <div className="video-gallery">
-            <h1>{cases.name}</h1>
-        
+
+            <div className="video-gallery">
+                <h1>{title}</h1>
             {videosGallery.map((image, index) => (
                 <div key={index}>
                     <Image image={image.url}/>
-                </div>
-        ))}
-        </div>
+                </div> ))}
+            </div>
     )
 }
 
