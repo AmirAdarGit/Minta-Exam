a663e075c11068258ce513e9e71588a76ec1075b
AmirAdarGit <amiradar12345@gmail.com>
2021-04-12 16:27:13 +0300

    extract UseCase and VideoGallery to Mints component

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit'
-import  useCasesSlice  from './useCasesSlice'
-import  videoGallerySlice  from './videoGalerySlice'
+import { configureStore } from "@reduxjs/toolkit";
+import useCasesSlice from "./useCasesSlice";
+import videoGallerySlice from "./videoGalerySlice";
+import { getDefaultMiddleware } from "@reduxjs/toolkit";
 
-export default configureStore({
+const middlewares = getDefaultMiddleware();
+
+const store = configureStore({
   reducer: {
-    "useCases": useCasesSlice,
-    "videoGallery": videoGallerySlice,
+    useCases: useCasesSlice,
+    videoGallery: videoGallerySlice,
+  },
+  // middlewares: {
+  //   loadFromLocalStorage: loadFromLocalStorage,
+  // },
+});
+
+store.subscribe(() => {
+  saveToLocalStorage(store.getState());
+});
+
+const loadFromLocalStorage = () => {
+  try {
+    if (localStorage[serializedData] === undefined) return undefined;
+    const serializedData = localStorage.getItem("state");
+
+    return JSON.stringify(serializedData);
+  } catch (err) {
+    console.log(err);
   }
-})
+};
+
+const saveToLocalStorage = (state) => {
+  try {
+    const serializedData = JSON.stringify(state);
+    localStorage.setItem("state", serializedData);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default store;
