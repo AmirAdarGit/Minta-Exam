7a0ccd6217c24abb0463da0c23a2b24b13dd0d24
Amir Adar <44618095+AmirAdarGit@users.noreply.github.com>
2021-04-20 17:27:37 +0300

    Update task-4.js

--- JS_Questions_&_Answers/task-4.js
+++ JS_Questions_&_Answers/task-4.js
@@ -1,5 +1,15 @@
+//solution 1
 for (let i = 0; i < 3; i++) {
   setTimeout(function () {
     alert(i);
   }, 1000 + i);
+}
+
+
+//another solution using closures
+
+for (var i = 0; i < 3; i++) {
+  setTimeout(function (theIth) {
+    return function () {alert(theIth)};
+  }(i), 1000 + i)
 }
